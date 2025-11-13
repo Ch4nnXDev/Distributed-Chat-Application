1499ee4d404ff2d71fab2bd2fc86b74b8a3bed3e
Channa2146@gmail.com <Channa2146@gmail.com>
2025-11-13 14:18:46 +0530

    ok

---
+++ frontend/src/hooks/isLoggedIn.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+const useIsLoggedIn = (req, res) => {
+    const [isLogged, setIsLogged] = useState("");
+    useEffect(()=> {
+        const checkStatus = () => {
+            
+        }
+
+    }, [])
+
+}
