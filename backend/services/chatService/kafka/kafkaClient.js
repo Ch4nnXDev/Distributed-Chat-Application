39c95ed3692534da4c5fd0a4b875f7af0b31e4f0
Channa2146@gmail.com <Channa2146@gmail.com>
2026-05-16 10:08:50 +0530

    docker implemented

--- backend/services/chatService/kafka/kafkaClient.js
+++ backend/services/chatService/kafka/kafkaClient.js
@@ -1,7 +1,7 @@
 const { Kafka } = require('kafkajs');
 const kafka = new Kafka({
     clientId: 'chat-service',
-    brokers: ['localhost:9092']
+    brokers: ['kafka:9092']
 })
 
 module.exports = kafka;
