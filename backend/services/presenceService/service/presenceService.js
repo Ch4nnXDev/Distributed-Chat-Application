ac92896b8656ac41bfeda56d6c0cb50e77949196
Channa2146@gmail.com <Channa2146@gmail.com>
2026-07-31 14:17:12 +0530

    prsenceService fucntions added

---
+++ backend/services/presenceService/service/presenceService.js
@@ -0,0 +1,29 @@
+import redis from "../config/redisClient";
+
+const createUser = async (user) => {
+    await redis.sAdd("online-users", user);
+    return console.log("User Created Inside Redis");
+
+}
+
+const getAllUser = async (id) => {
+
+    const users = await redis.sMembers("online-users");
+    return users;
+
+}
+
+const checkOnline = async (user) => {
+    const isOnline = await redis.sIsMember("online-users", user);
+    if (isOnline) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+
+module.exports = {
+    createUser, getAllUser, checkOnline
+}
