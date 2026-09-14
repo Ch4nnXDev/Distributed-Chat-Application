8800c9b67cfee3f3c31e057056c1529b7367c4c8
Channa2146@gmail.com <Channa2146@gmail.com>
2026-09-14 13:54:35 +0530

    conversation data model added

--- backend/services/chatService/models/conversationModel.js
+++ backend/services/chatService/models/conversationModel.js
@@ -3,6 +3,21 @@ const {required} = require('@hapi/joi');
 
 
 const conversationModel = new mongoose.Schema({
-    
+    participants: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: "User",
+        required: true
+    },
 
-})
+    texts: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: "Message",
+        required: false
+    }
+
+
+
+}, { timestamps: true});
+
+const Conversation = mongoose.model('Conversation', conversationModel, 'conversations');
+module.exports = Conversation;
