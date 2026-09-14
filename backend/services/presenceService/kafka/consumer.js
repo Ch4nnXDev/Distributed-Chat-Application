db54f10c2e986ada1ddfc1b61f743c394240d3bd
Channa2146@gmail.com <Channa2146@gmail.com>
2026-09-14 14:01:47 +0530

    implemented the presenceService for Redis Operations

--- backend/services/presenceService/kafka/consumer.js
+++ backend/services/presenceService/kafka/consumer.js
@@ -1,16 +1,50 @@
 const kafka = require("./kafkaClient");
-const consumer = kafka.consumer();
+const {
+    createUserController,
+    getAllUsersController,
+    isOnlineController,
+    userOffline
+} = require("../controllers/presenceController");
+
+const consumer = kafka.consumer({
+    groupId: "presence-group"
+});
 
 
-const startConsumer = async () => {
+const startConsumer = async (topic) => {
 
     try {
         await consumer.connect();
-        console.log("Consumer Started")
+        console.log("Consumer Started");
+
+        await consumer.subscribe({
+            topic: topic,
+            fromBeginning: false
+
+        });
+
+        await consumer.run({
+            eachMessage: async ({ topic, partition, message }) => {
+
+                const data = JSON.parse(
+                    message.value.toString()
+                );
+                switch (data.type) {
+                    case "USER_ONLINE":
+                        await createUserController(data.userId)
+                }
+
+                console.log("Received event:", data);
+            }
+        });
 
     } catch (error) {
         console.log("Error Starting The Consumer", error)
     }
     
 
+}
+
+module.exports = {
+    startConsumer
 }
