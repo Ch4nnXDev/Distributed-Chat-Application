510c8098c3558d57d26dccda5783231f6558516f
Channa2146@gmail.com <Channa2146@gmail.com>
2026-09-15 09:37:19 +0530

    user param to userId param

--- backend/services/presenceService/service/presenceService.js
+++ backend/services/presenceService/service/presenceService.js
@@ -1,29 +1,36 @@
-const redis = require("../config/redisClient");
+const { redisClient: redis } = require("../config/redisClient");
 
-const createUser = async (user) => {
-    await redis.sAdd("online-users", user);
-    return console.log("User Created Inside Redis");
+const createUser = async (userId) => {
+    await redis.sAdd("online-users", userId);
+    console.log("User Created Inside Redis");
 
 }
 
-const getAllUser = async (id) => {
+const getAllUsers = async () => {
 
     const users = await redis.sMembers("online-users");
     return users;
 
 }
 
-const checkOnline = async (user) => {
-    const isOnline = await redis.sIsMember("online-users", user);
-    if (isOnline) {
-        return true;
-    } else {
-        return false;
-    }
+const checkOnline = async (userId) => {
+    return await redis.sIsMember("online-users", userId);
+    
+    
 
 }
 
+const userOffline = async (userId) => {
+    return await redis.sRem("online-users", userId);
+};
+
+
+const getOnlineCount = async () => {
+    return await redis.sCard("online-users");
+
+};
+
 
 module.exports = {
-    createUser, getAllUser, checkOnline
+    createUser, getAllUsers, checkOnline, userOffline, getOnlineCount
 }
