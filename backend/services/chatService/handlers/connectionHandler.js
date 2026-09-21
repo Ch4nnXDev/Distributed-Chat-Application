649be153798473a87ce9e867f827b63567a1e210
Channa2146@gmail.com <Channa2146@gmail.com>
2026-09-21 10:41:17 +0530

    changed the kafka event sender with presenceHandler

--- backend/services/chatService/handlers/connectionHandler.js
+++ backend/services/chatService/handlers/connectionHandler.js
@@ -1,16 +1,17 @@
 const { messageHandler } = require("./messageHandler")
-const { sendMessage } = require("./../kafka/producer");
-const { PRESENCE_EVENTS } = require("../kafka/topics");
 const { presenceHandler } = require("./connectionHandler");
+
+
 
 const connectionHandler = async (socket, io) => {
 
     console.log('User connected:', socket.user.id, 'as', socket.user?.email);
-    
-    await sendMessage(PRESENCE_EVENTS, {
-        type: "USER_ONLINE",
-        userId: socket.user.id,
-        emailId: socket.user.email
+
+    const userId = socket.user.id;
+
+    await presenceHandler({
+        userId: userId,
+        status: "online"
     });
 
 
@@ -21,10 +22,9 @@ const connectionHandler = async (socket, io) => {
         console.log('User disconnected:', socket.id);
 
 
-        await sendMessage(PRESENCE_EVENTS, {
-            type: "USER_OFFLINE",
-            userId: socket.user.id,
-            emailId: socket.user.email
+        await presenceHandler({
+            userId: userId,
+            status: "offline"
         })
     });
 
