d7a31297fdcb7319ef8a7a12b062db25e2b3b553
Channa2146@gmail.com <Channa2146@gmail.com>
2026-09-22 09:11:25 +0530

    added the convesation handler

--- backend/services/chatService/handlers/conversationHandler.js
+++ backend/services/chatService/handlers/conversationHandler.js
@@ -1,9 +1,19 @@
 const conversationHandler = (socket, io) => {
+
     socket.on("join_conversation", async (conversationId) => {
         try {
+
+            socket.join(conversationId);
+
+            console.log(
+                `User ${socket.user.id} joined conversation ${conversationId}`
+            );
 
         } catch (err) {
             console.error("Error joining the conversation", err);
         }
-    })
-}
+    });
+
+};
+
+module.exports = { conversationHandler };
